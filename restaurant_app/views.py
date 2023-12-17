5c9bce44c0db3621d97c3cd82c6055f597c73d2d
Arek Sanecki <areksanecki@gmail.com>
2023-12-17 23:12:25 +0100

    Test podpięcia bazy danych do pliku html

--- restaurant_app/views.py
+++ restaurant_app/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from store.models import Product
 
 
 def home(request):
-    return render(request, 'home.html')
+    beverages = Product.objects.all().filter(is_available=True).order_by('id')
+    context = {
+        'beverages': beverages,
+    }
+    
+    return render(request, 'home.html', context)
