744b68afe12826511adec2e728780d4072e506c1
Arek Sanecki <areksanecki@gmail.com>
2023-12-20 22:42:05 +0100

    dodanie clean() w forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -24,3 +24,21 @@ class RegistrationForm(forms.ModelForm):
         self.fields['email'].widget.attrs['placeholder']= 'Podaj Adres Email'
         for field in self.fields:
             self.fields[field].widget.attrs['class']= 'form-control'
+    
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+        
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "Hasła nie są takie same!"
+            )
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Podaj Imię'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Podaj Nazwisko'
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'Podaj Numer Telefonu'
+        self.fields['email'].widget.attrs['placeholder'] = 'Podaj Adres Email'
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
